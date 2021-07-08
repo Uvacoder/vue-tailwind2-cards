8da9ae7c42de4a3f32dc03b20d2ac2e1a09e9b7b
KingSit3 <i.ardiansyah93@gmail.com>
2021-07-08 11:19:29 +0700

    Add Admin 2

---
+++ src/views/Admin/Admin2.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="bg-[#1F2937] w-full min-h-screen max-h-full pb-3 font-sans flex text-white">
+    <!-- Side Nav -->
+      <div class="p-6 w-[15%]">
+        <!-- Logo -->
+          <div class="font-bold text-white text-2xl flex justify-center">
+            <img class="w-10 mr-3" src="../../assets/images/myponyasia/logo-square.png" alt="Image" />
+            Ad<span class="text-green-500">min</span>
+          </div>
+          <!-- End Logo -->
+
+          <!-- Menus -->
+          <div class="mt-10">
+          <a href="#">
+            <div class="text-green-400 font-semibold hover:bg-trueGray-700 duration-200 rounded-lg">
+              <div class="flex pl-2 py-1 items-center space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>
+                <span>
+                  Dashboard
+                </span>
+              </div>
+            </div>
+          </a>
+          </div>
+          <div class="mt-2">
+            <a href="#">
+              <div class="text-green-400 font-semibold hover:bg-trueGray-700 duration-200 rounded-lg">
+                <div class="flex pl-2 py-1 items-center space-x-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                  </svg>
+                  <span>
+                    Statistik
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="mt-2">
+          <a href="#">
+            <div class="text-green-400 font-semibold hover:bg-trueGray-700 duration-200 rounded-lg">
+              <div class="flex pl-2 py-1 items-center space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>
+                <span>
+                  Dashboard
+                </span>
+              </div>
+            </div>
+          </a>
+          </div>
+          <div class="mt-2">
+          <a href="#">
+            <div class="text-green-400 font-semibold hover:bg-trueGray-700 duration-200 rounded-lg">
+              <div class="flex pl-2 py-1 items-center space-x-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>
+                <span>
+                  Dashboard
+                </span>
+              </div>
+            </div>
+          </a>
+          </div>
+          <!-- End Menus -->
+
+      </div>
+    <!-- End Side Nav -->
+
+    <!-- Right Content -->
+      <div class="bg-[#334155] w-[85%] m-5 rounded-xl p-3">
+
+        <!-- TopBar -->
+          <div class="flex justify-between px-2">
+            <span class="font-bold text-xl"> Dashboard </span>
+            <div class="relative flex">
+              <input type="text" placeholder="Search" class="pl-8 pr-3 py-1 bg-[#1F2937] rounded-lg focus:ring-2 focus:outline-none outline-none duration-100 ring-green-500">
+              <svg class="w-5 absolute left-1.5 top-1.5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </div>
+          </div>
+        <!-- End TopBar -->
+
+        <!-- Content -->
+        <div class="mt-3 px-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, quod accusantium ab omnis illum reiciendis quas voluptas, commodi culpa debitis ea officia molestias. Iusto, molestiae atque! Modi optio tempore nemo nam quo doloremque odio ullam eos aspernatur aut necessitatibus voluptatem suscipit vel, cumque eveniet similique placeat officiis dolor delectus, dolore nulla expedita omnis id. Vitae magnam quam commodi tenetur doloremque impedit odit placeat, asperiores assumenda eveniet, error qui esse, perferendis modi quo alias. Odit, aut quidem, amet dicta soluta quibusdam blanditiis quaerat aliquam esse eos libero. Recusandae commodi atque quis eius eos illo quo provident, perspiciatis, corrupti officia saepe blanditiis.
+        </div>
+        <!-- End Content -->
+
+      </div>
+    <!-- End Right Content -->
+  </div>
+</template>
