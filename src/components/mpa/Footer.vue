138dab087c7674d7a143f6ae5510b096aaa38d59
KingSit3 <i.ardiansyah93@gmail.com>
2021-07-16 11:20:52 +0700

    Split to component

---
+++ src/components/mpa/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Footer -->
+      <div class="flex justify-center px-10 pt-2 bg-black shadow-2xl">
+        <div class="flex flex-col items-center justify-center ">
+          <span class="font-bold text-lg mb-3 cursor-default">Partners</span>
+          <div class="flex space-x-4">
+            <a href="/myAnimeAsia/detail"><img class="w-28 rounded-xl hover:opacity-75 duration-150" src="../../assets/images/myponyasia/landscape1.png" alt="Partners Banner" /></a>
+            <a href="/myAnimeAsia/detail"><img class="w-28 rounded-xl hover:opacity-75 duration-150" src="../../assets/images/myponyasia/landscape1.png" alt="Partners Banner" /></a>
+            <a href="/myAnimeAsia/detail"><img class="w-28 rounded-xl hover:opacity-75 duration-150" src="../../assets/images/myponyasia/landscape1.png" alt="Partners Banner" /></a>
+          </div>
+          <div class="grid grid-cols-2 gap-3 sm:flex sm:flex-row  mt-3 text-center">
+            <a href="/myAnimeAsia/detail" class="mx-5 hover:opacity-60 duration-150">Privacy Policy</a>
+            <span class="hidden sm:contents">&bull;</span>
+            <a href="/myAnimeAsia/detail" class="mx-5 hover:opacity-60 duration-150">FeedBack</a>
+            <span class="hidden sm:contents">&bull;</span>
+            <a href="/myAnimeAsia/detail" class="mx-5 hover:opacity-60 duration-150">Support Us</a>
+            <span class="hidden sm:contents">&bull;</span>
+            <a href="/myAnimeAsia/detail" class="mx-5 hover:opacity-60 duration-150">Contact Us</a>
+          </div>
+          <div class="border border-b-2 border-trueGray-700 border-opacity-75 w-full mt-2"></div>
+          <div class="text-center bg-black pb-5 mt-2 text-sm">My Pony Asia is in no way associated with Hasbro or any author of the series My Little Pony Friendship is Magic and its derivatives.</div>
+        </div>
+      </div>
+    <!-- End Footer -->
+</template>
---
+++ src/components/mpa/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Navbar -->
+      <div class="flex justify-start bg-trueGray-900 p-4 rounded-b shadow-xl">
+        <div>
+          <a href="/myAnimeAsia/homepage">
+            <img class="w-40" src="../../assets/images/myponyasia/logo-myponyasia.png" alt="" />
+          </a>
+        </div>
+      </div>
+    <!-- End Navbar -->
+</template>
