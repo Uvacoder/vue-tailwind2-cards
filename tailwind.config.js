f0d49ccee25c7419d4da93d92523f1302ac2d30a
KingSit3 <i.ardiansyah93@gmail.com>
2021-07-18 22:48:39 +0700

    update lagi :v

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   purge: [
-    // './index.html', 
-    // './src/**/*.{vue,js,ts,jsx,tsx}'
+    './index.html', 
+    './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
