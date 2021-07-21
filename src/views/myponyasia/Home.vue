4f6c92187ad986e63d349d1a5998bed8e3702037
KingSit3 <i.ardiansyah93@gmail.com>
2021-07-21 08:24:29 +0700

    add Corousel Functionality

--- src/views/myponyasia/Home.vue
+++ src/views/myponyasia/Home.vue
@@ -8,19 +8,50 @@
     <!-- End Navbar -->
 
     <!-- Corousel Section -->
-    <div class="relative flex flex-col lg:justify-center lg:flex-row w-full bg-black h-72 md:h-[350px] lg:h-96">
-        
-        <!-- Corousel -->
+      <swiper
+      :options="swiperOptions"
+      :slides-per-view="auto"
+      navigation
+      lazy
+      loop
+      autoPlay
+      :pagination="{ clickable: true, type: 'bullets'}"
+      class="h-72 md:h-[350px] lg:h-96"
+      >
+        <swiper-slide class="relative flex flex-col lg:justify-center lg:flex-row w-full bg-black h-72 md:h-[350px] lg:h-96">
+          <!-- Corousel -->
         <img class="w-full h-full lg:h-full object-cover opacity-70 lg:opacity-30" src="../../assets/images/myponyasia/landscape1.png" alt="Corousel">
         <div class="lg:absolute w-full h-full lg:h-96 flex flex-col lg:justify-center items-center py-5 lg:py-0 font-semibold">
-          <p class="text-base md:text-2xl lg:text-3xl cursor-default capitalize text-center">Gotoubun no hanayome season 2 is out!</p>
+          <p class="text-base md:text-2xl lg:text-3xl cursor-default capitalize text-center px-10 line-clamp-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum modi, perspiciatis quos aliquid ipsam sunt fuga? Distinctio deleniti possimus amet veniam quae natus dicta aut repellat facere illo, maxime saepe.</p>
           <p class="cursor-default capitalize text-center font-normal truncate w-3/4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quas expedita consectetur, perferendis eveniet est deleniti esse sit quae. Doloremque facilis, modi earum ipsum, illum odio alias ullam natus dolor maiores deserunt tempore placeat dolores ducimus quaerat nostrum cupiditate eum!</p>
-          <a href="#" class="ring-2 ring-white p-2 rounded-lg mt-5 hover:bg-white hover:bg-opacity-75 hover:ring-opacity-75 hover:text-black duration-150">Click Here</a>
+          <a href="#" class="ring-2 ring-white p-2 rounded-lg mt-4 mb-3 hover:bg-white hover:bg-opacity-75 hover:ring-opacity-75 hover:text-black duration-150">Click Here</a>
         </div>
         <!-- End Corousel -->
+        </swiper-slide>
+        <swiper-slide class="relative flex flex-col lg:justify-center lg:flex-row w-full bg-black h-72 md:h-[350px] lg:h-96">
+          <!-- Corousel -->
+        <img class="w-full h-full lg:h-full object-cover opacity-70 lg:opacity-30" src="../../assets/images/myponyasia/landscape2.png" alt="Corousel">
+        <div class="lg:absolute w-full h-full lg:h-96 flex flex-col lg:justify-center items-center py-5 lg:py-0 font-semibold">
+          <p class="text-base md:text-2xl lg:text-3xl cursor-default capitalize text-center px-10 line-clamp-3">Gotoubun no hanayome season 2 is out!</p>
+          <p class="cursor-default capitalize text-center font-normal truncate w-3/4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quas expedita consectetur, perferendis eveniet est deleniti esse sit quae. Doloremque facilis, modi earum ipsum, illum odio alias ullam natus dolor maiores deserunt tempore placeat dolores ducimus quaerat nostrum cupiditate eum!</p>
+          <a href="#" class="ring-2 ring-white p-2 rounded-lg mt-4 mb-3 hover:bg-white hover:bg-opacity-75 hover:ring-opacity-75 hover:text-black duration-150">Click Here</a>
+        </div>
+        <!-- End Corousel -->
+        </swiper-slide>
+        <swiper-slide class="relative flex flex-col lg:justify-center lg:flex-row w-full bg-black h-72 md:h-[350px] lg:h-96">
+          <!-- Corousel -->
+        <img class="w-full h-full lg:h-full object-cover opacity-70 lg:opacity-30" src="../../assets/images/myponyasia/poster-portrait.webp" alt="Corousel">
+        <div class="lg:absolute w-full h-full lg:h-96 flex flex-col lg:justify-center items-center py-5 lg:py-0 font-semibold">
+          <p class="text-base md:text-2xl lg:text-3xl cursor-default capitalize text-center px-10 line-clamp-3">Gotoubun no hanayome season 2 is out!</p>
+          <p class="cursor-default capitalize text-center font-normal truncate w-3/4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quas expedita consectetur, perferendis eveniet est deleniti esse sit quae. Doloremque facilis, modi earum ipsum, illum odio alias ullam natus dolor maiores deserunt tempore placeat dolores ducimus quaerat nostrum cupiditate eum!</p>
+          <a href="#" class="ring-2 ring-white p-2 rounded-lg mt-4 mb-3 hover:bg-white hover:bg-opacity-75 hover:ring-opacity-75 hover:text-black duration-150">Click Here</a>
+        </div>
+        <!-- End Corousel -->
+        </swiper-slide>
+      </swiper>
 
         <!-- Button -->
-        <button class="absolute top-[40%] right-5 lg:right-10">
+        <!-- <button class="absolute top-[40%] right-5 lg:right-10">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 md:h-16 w-7 md:w-16 rounded-full bg-trueGray-600 bg-opacity-30 hover:bg-opacity-90 duration-150" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
           </svg>
@@ -29,20 +60,14 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 md:h-16 w-7 md:w-16 rounded-full bg-trueGray-600 bg-opacity-30 hover:bg-opacity-90 duration-150" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
           </svg>
-        </button>
+        </button> -->
         <!-- End Button -->
 
         <!-- Progress bar Corousel -->
-        <div class="static lg:absolute bottom-1 items-center justify-center flex space-x-2">
+        <!-- <div class="pbar static lg:absolute bottom-1 items-center justify-center flex space-x-2">
           <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-          <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-          <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-          <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-          <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-          <div class="w-6 md:w-9 lg:w-12 bg-blue-700 bg-opacity-80 h-1" />
-        </div>
+        </div>  -->
         <!-- End Progress bar Corousel -->
-    </div>
     <!-- End Corousel Section -->
 
     <!-- On Progress Section -->
@@ -94,8 +119,8 @@
               <AnimeCardSkeleton />
             </div>
         </div>
-        <div v-show="loadingState == false" class="flex justify-end">
-            <router-link to="/myponyasia/anime" class=" mr-5 p-2 bg-blue-700 font-semibold rounded-lg hover:bg-blue-800 duration-200">See More</router-link>
+        <div v-show="loadingState == false" class="flex justify-center">
+            <router-link to="/myponyasia/anime2" class="p-2 px-7 bg-trueGray-600 font-semibold rounded-lg hover:bg-blue-700 duration-200">See More</router-link>
         </div>
         
     </div>
@@ -109,10 +134,20 @@
 
 <script>
 import {ref} from 'vue'
+import SwiperCore, { Navigation, Pagination} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
 import Header from '../../components/mpa/Header.vue'
 import Footer from '../../components/mpa/Footer.vue'
 import AnimeCard from '../../components/mpa/AnimeCard.vue'
 import AnimeCardSkeleton from '../../components/mpa/parts/AnimeCardSkeleton.vue'
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/lazy/lazy.scss';
+
+SwiperCore.use([Navigation, Pagination]);
 
 export default {
   props: {
@@ -122,11 +157,22 @@ export default {
     }
   },
 
+  data() {
+    return {
+      swiperOptions:{
+        Autoplay: true,
+        pagination: true,
+      }
+    }
+  },
+
   components:{
     Header,
     Footer,
     AnimeCard,
     AnimeCardSkeleton,
+    Swiper,
+    SwiperSlide,
   },
 
   setup(){
