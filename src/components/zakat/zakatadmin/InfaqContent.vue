0e9a3d264133b8b9e1d352d3186edc9e9dd4ee05
KingSit <i.ardiansyah93@gmail.com>
2021-09-28 11:17:20 +0700

    Init commit

---
+++ src/components/zakat/zakatadmin/InfaqContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">Ini Content Infaq</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/zakat/zakatadmin/MustahikContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">Ini Content Mustahik</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
