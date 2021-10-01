764dc7ccecceff989f57be38ca7ae23aa4af6a14
KingSit <i.ardiansyah93@gmail.com>
2021-10-01 22:55:22 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import store from './store'
 import router from './router'
 
 createApp(App)
    .use(router)
+   .use(store)
    .mount('#app')
