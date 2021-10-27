3e15dcf423dbf3d49edec5dfbb963e250a960f19
kingsit3 <i.ardiansyah93@gmail.com>
2021-10-27 17:37:49 +0700

    minor changes

--- src/components/zakat/zakatadmin/Sidebar.vue
+++ src/components/zakat/zakatadmin/Sidebar.vue
@@ -29,9 +29,11 @@
     <router-link to="/zakatadmin/mustahik" :class="[ this.$route.fullPath == '/zakatadmin/mustahik' ? 'text-blue-500/90 bg-white' : 'text-white/50 hover:text-white/100 duration-150']" class="flex p-1.5 px-5 rounded-full items-center space-x-2 duration-200">
       <h1 class="text-lg font-bold">Daftar Mustahik</h1>
     </router-link>
-    <router-link to="/zakatadmin/admins" :class="[ this.$route.fullPath == '/zakatadmin/admins' ? 'text-blue-500/90 bg-white' : 'text-white/50 hover:text-white/100 duration-150']" class="flex p-1.5 px-5 rounded-full items-center space-x-2 duration-200">
-      <h1 class="text-lg font-bold">Admin</h1>
-    </router-link>
+    <div v-show="role == 'admin' ">
+      <router-link to="/zakatadmin/admins" :class="[ this.$route.fullPath == '/zakatadmin/admins' ? 'text-blue-500/90 bg-white' : 'text-white/50 hover:text-white/100 duration-150']" class="flex p-1.5 px-5 rounded-full items-center space-x-2 duration-200">
+        <h1 class="text-lg font-bold">Admin</h1>
+      </router-link>
+    </div>
     <!-- Zakat Fitrah menu -->
 
   </div>
@@ -60,7 +62,8 @@ axios.defaults.timeout = 5000
 export default {
   data() {
     return {
-      user: localStorage.getItem('nama')
+      user: localStorage.getItem('nama'),
+      role: localStorage.getItem('role')
     }
   },
   methods: {
